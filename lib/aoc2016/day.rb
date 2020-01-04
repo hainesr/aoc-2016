19ddf5443340ba4893cb2a874a3fb99a3e00362c
Robert Haines <rhaines@manchester.ac.uk>
2020-01-04 15:02:34 +0000

    Refactor the puzzle infrastructure to add timings.
    Now does timing automatically and for each part individually.

--- lib/aoc2016/day.rb
+++ lib/aoc2016/day.rb
@@ -6,10 +6,32 @@
 #
 # Public Domain
 
+require 'benchmark'
+
 module AOC2016
   class Day
+    FORMAT = '%n ' + Benchmark::FORMAT
+
+    def setup; end
+
     def run
-      puts "This day's puzzle hasn't been solved yet."
+      setup_time = Benchmark.measure('  Setup:') { setup }
+      part1_time = Benchmark.measure(' Part 1:') { part1 }
+      part2_time = Benchmark.measure(' Part 2:') { part2 }
+
+      puts
+      puts 'Timings: ' + Benchmark::CAPTION
+      puts setup_time.format(FORMAT)
+      puts part1_time.format(FORMAT)
+      puts part2_time.format(FORMAT)
+    end
+
+    def part1
+      puts "Part 1: hasn't been completed yet."
+    end
+
+    def part2
+      puts "Part 2: hasn't been completed yet."
     end
 
     private
@@ -24,7 +46,8 @@ module AOC2016
     end
 
     def class_snake_case
-      ::File.basename(method(:run).source_location[0]).split('.')[0]
+      filename = Object.const_source_location(self.class.to_s)[0]
+      ::File.basename(filename).split('.')[0]
     end
   end
 end
